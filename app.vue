577c6e1bfe3be4cd039b0f5b6168bcc90989034a
deevlas <deevlas@icloud.com>
2020-09-14 14:06:53 +0300

    finish vue-2

---
+++ app.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <addToDo :toDoArr="toDoArr" @addToDoApp="addToDoList" />
+    <todolist :toDoArr="toDoArr" @removeToDoApp="remToDo" />
+  </div>
+</template>
+
+<script>
+import todolist from "./components/todo/todolist";
+import addToDo from "./components/todo/addToDo";
+
+export default {
+  name: "App",
+
+  data() {
+    return {
+      toDoArr: []
+    };
+  },
+
+  components: {
+    addToDo,
+    todolist
+  },
+
+  methods: {
+    remToDo(index) {
+      this.toDoArr.splice(index, 1);
+    },
+    addToDoList(txt) {
+      if (txt.length > 0) {
+        this.toDoArr.push(txt);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
